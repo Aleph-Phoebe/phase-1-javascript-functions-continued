315cf11438cda7fe6ce173e41780fdbc01585f3d
Aleph-Phoebe <phbwangui47@gmail.com>
2024-06-30 10:09:49 +0300

    COMPLETED

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // code your solution here
+function saturdayFun(activity = "roller-skate") {
+    return `This Saturday, I want to ${activity}!`;
+}
+
+//Activity
+function mondayWork(activity = "go to the office") {
+    return `This Monday, I will ${activity}.`;
+}
+
+//Symbol
+function wrapAdjective(symbol = "*") {
+    return function(adjective) {
+        return `You are ${symbol}${adjective}${symbol}!`;
+    }
+}
+
